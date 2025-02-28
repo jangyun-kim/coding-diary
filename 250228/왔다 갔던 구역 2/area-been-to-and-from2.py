99279b0760be2b45783b7df3156e88b26f884bc0
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2025-02-28 16:55:12 +0900

    Title: 왔다 갔던 구역 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250228/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250228/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,14 +1,16 @@
 N = int(input())
 
+# dictionary to record visited position
+visited = {}
+
 # Initial position
 current_position = 0
+visited[current_position] = 0
 
-visited = [0] * 200001
+# list to record the pathway
+path = [current_position]
 
-# Calculate an overlaped range
-overlap_count = 0
-
-# Movement process by a command
+# follow visited position
 for _ in range(N):
     x, direction = input().split()
     x = int(x)
@@ -17,18 +19,23 @@ for _ in range(N):
     if direction == 'L':
         for i in range(x):
             current_position -= 1
-            if visited[current_position + 100000] == 1:
-                overlap_count += 1
-            visited[current_position + 100000] += 1
+            if current_position in visited:
+                visited[current_position] += 1
+            else:
+                visited[current_position] = 1
 
     # Move to 'Right'
     elif direction == 'R':
         for i in range(x):
             current_position += 1
-            if visited[current_position + 100000] == 1:
-                overlap_count += 1
-            visited[current_position + 100000] += 1
+            if current_position in visited:
+                visited[current_position] += 1
+            else:
+                visited[current_position] = 1
+
+overlap_count = 0
+for count in visited.values():
+    if count >= 2:
+        overlap_count += 1
 
 print(overlap_count)
-    
-
