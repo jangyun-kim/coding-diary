c5d5e47b2a0de9e0bf2924119a7c25a85a620162
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2025-02-28 16:57:46 +0900

    Title: 왔다 갔던 구역 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250228/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250228/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,41 +1,41 @@
+# N (명령의 수) 입력
 N = int(input())
 
-# dictionary to record visited position
+# 위치를 방문한 횟수를 기록할 딕셔너리
 visited = {}
 
-# Initial position
+# 시작 위치
 current_position = 0
-visited[current_position] = 0
 
-# list to record the pathway
-path = [current_position]
-
-# follow visited position
+# 명령에 따른 이동 처리
 for _ in range(N):
     x, direction = input().split()
-    x = int(x)
-
-    # Move to 'Left'
+    x = int(x)  # 이동할 크기
+    
     if direction == 'L':
-        for i in range(x):
+        # 왼쪽으로 x만큼 이동
+        for _ in range(x):
             current_position -= 1
+            # 위치 방문 횟수 증가
             if current_position in visited:
                 visited[current_position] += 1
             else:
                 visited[current_position] = 1
-
-    # Move to 'Right'
     elif direction == 'R':
-        for i in range(x):
+        # 오른쪽으로 x만큼 이동
+        for _ in range(x):
             current_position += 1
+            # 위치 방문 횟수 증가
             if current_position in visited:
                 visited[current_position] += 1
             else:
                 visited[current_position] = 1
 
+# 2번 이상 지나간 구간의 크기 계산
 overlap_count = 0
 for count in visited.values():
     if count >= 2:
         overlap_count += 1
 
+# 2번 이상 지나간 영역의 크기 출력
 print(overlap_count)
