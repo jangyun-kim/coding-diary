ecb7b7277e477ded54eb62e05b514fee24d59e68
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2025-03-04 17:43:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250304/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250304/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -14,14 +14,18 @@ def min_covering_rectangle(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
     ##첫 번째 직사각형에서 두 번째 직사각형이 덮지 못한 잔여 영역을 최소 직사각형으로 덮는 크기를 계산
     intersection = intersection_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)
     remaining_area = area(ax1, ay1, ax2, ay2) - intersection
-    return remaining_area
+
+    if min(ay2, by2) - max(ay2, by2) == (ay2 - ay1):
+        return remaining_area
+    else:
+        return remaining_area + intersection
 
 # 입력
 ax1, ay1, ax2, ay2 = map(int, input().split())
 bx1, by1, bx2, by2 = map(int, input().split())
 
 # 최소 직사각형의 넓이 계산
-result = min_covering_rectangle(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) + intersection_area(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)
+result = min_covering_rectangle(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)
 
 # 결과 출력
 print(result)
