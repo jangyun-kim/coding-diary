dd672ad911866c94f71194445ed7081bead9bf67
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2025-03-28 21:32:16 +0900

    Title: 이상한 폭탄 2, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250328/이상한 폭탄 2/strange-bomb-2.py
+++ 250328/이상한 폭탄 2/strange-bomb-2.py
@@ -1,11 +1,12 @@
 N, K = map(int, input().split())
 boombs = [int(input()) for _ in range(N)]
 
-boom = 0
+boom = -1
 for i in range(N):
     for j in range(i + 1, min(i + K + 1, N)):
         if boombs[i] == boombs[j]:
             boom = max(boom, boombs[i])
+
 
 print(boom)
 
