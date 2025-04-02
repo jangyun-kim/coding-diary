427b009e961dc2b87fa0e92229c81ddcc336564f
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2025-04-02 13:48:08 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250402/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#define MAX_N 100
+#define MAX_K 100
+
+using namespace std;
+
+int N;
+int arr[MAX_N];
+int k_count[MAX_K + 1] = {0};
+int k;
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int total = arr[i] + arr[j];
+            if (total % 2 != 0) {
+                continue;
+            }
+
+            k = total / 2;
+            if (k <= MAX_K) {
+                k_count[k]++;
+            }
+        }
+    }
+
+    int max_count = 0;
+    for (int i = 0; i <= MAX_K; i++) {
+        if (k_count[i] > max_count) {
+            max_count = k_count[k];
+        }
+    }
+
+    cout << max_count;
+
+    return 0;
+}
